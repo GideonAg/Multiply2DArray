7ee6cc279bd4d27dbd4194ea5dd6628ffdb2759b
GideonAg <gideonagbosu72@gmail.com>
2022-07-03 13:07:45 +0000

    Modified Main.java
    Modified the multiply function to handle the conformity of the arrays passed in and made it more efficient.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,28 +6,31 @@ public class Main {
         int [][] array1 = {{1, 2, 3}, {4, 5, 6}};
         int [][] array2 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
 
-        if (array1[0].length != array2.length){
-            System.out.println("Multiplication undefined");
-        }else {
-            int [][] result = multiply(array1, array2);
-            for (int[] ints : result) {
-                for (int column = 0; column < result[0].length; column++) {
-                    System.out.print(ints[column] + " ");
-                }
-                System.out.println();
-            }
-        }
+        multiply(array1, array2);
+
     }
 
-    public static int [][] multiply(int [][] arrayOne, int [][] arrayTwo){
+    public static void multiply(int [][] arrayOne, int [][] arrayTwo){
         int [][] answer = new int[arrayOne.length][arrayTwo[0].length];
-        for (int row = 0; row < arrayOne.length; row++){
-            for (int column = 0; column < arrayTwo[0].length; column++){
-                for (int i = 0; i < arrayOne[0].length; i++){
-                    answer[row][column] += arrayOne[row][i] * arrayTwo[i][column];
+
+        if(arrayOne[0].length != arrayTwo.length){
+            System.out.println("Multiplication undefined");
+        }else {
+            for (int row = 0; row < arrayOne.length; row++){
+                for (int column = 0; column < arrayTwo[0].length; column++){
+                    for (int i = 0; i < arrayOne[0].length; i++){
+                        answer[row][column] += arrayOne[row][i] * arrayTwo[i][column];
+                    }
                 }
             }
         }
-        return answer;
+
+        for (int [] row : answer){
+            for(int column : row){
+                System.out.print(column + " ");
+            }
+            System.out.println();
+        }
+
     }
 }
